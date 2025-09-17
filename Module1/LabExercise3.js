13708bca7c6a9846048bd294f688cca2df77238b
ljennings119 <laurenjennings119@gmail.com>
2025-09-17 17:32:53 -0500

    world

---
+++ Module1/LabExercise3.js
@@ -0,0 +1,16 @@
+
+    function hello(){
+        alert('HIGH FIVE!');
+        document.getElementById("heading1").innerHTML = "Que Paso?";
+    document.getElementById("column1").style.backgroundColor = "lightblue";
+document.getElementById("input1").value = "How are you?"
+    }
+
+
+     
+    function world(){
+        alert('EARTH DAY!');
+        document.getElementById("heading2").innerHTML = "Save the Earth!";
+    document.getElementById("column2").style.backgroundColor = "lightgreen";
+document.getElementById("input2").value = "Reduce, Reuse, Recycle"
+    }
